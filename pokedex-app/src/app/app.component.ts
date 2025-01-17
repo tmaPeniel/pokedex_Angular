d732f4e07e9de32bf7508ecbb036924710d5ded6
tmaPeniel <taouemap@gmail.com>
2025-01-17 11:41:53 +0100

    Template

--- pokedex-app/src/app/app.component.ts
+++ pokedex-app/src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  templateUrl: 'app.component.html',
+  standalone: false
 })
-export class AppComponent {
-  title = 'pokedex-app';
+export class AppComponent implements OnInit {
+  pokemons : Pokemon[] = POKEMONS;
+  pokemonSelected : Pokemon | undefined;
+
+  ngOnInit() {
+      console.table(this.pokemons);
+  }
+
+  selectPokemon(pokemonId: string){
+    const pokemon : Pokemon | undefined = this.pokemons.find(pokemon=> pokemon.id == +pokemonId);
+    if(pokemon){
+      console.log(`Vous avez selectionné : ${pokemon.name}. Il a ${pokemon.hp} point de vie.`);
+      this.pokemonSelected = pokemon;
+    }
+    else{
+      console.log(`Vous avez selectionné un pokemon  qui n'existe pas.`);
+      this.pokemonSelected = pokemon;
+    }
+  }
 }
