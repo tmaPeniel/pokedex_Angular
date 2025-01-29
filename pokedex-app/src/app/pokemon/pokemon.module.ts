0e11d6856296c235be0a4ca98eed412452a72a12
tmaPeniel <taouemap@gmail.com>
2025-01-29 09:29:07 +0100

    Services

--- pokedex-app/src/app/pokemon/pokemon.module.ts
+++ pokedex-app/src/app/pokemon/pokemon.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonService } from './pokemon.service';
 
 
 const routes: Routes = [
@@ -21,7 +23,9 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
-  ]
+  ],
+  providers: [PokemonService]
 })
 export class PokemonModule { }
