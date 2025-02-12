ade8c4ecd2710a4c96a84b659e0e10e55cd8971d
tmaPeniel <taouemap@gmail.com>
2025-02-12 11:11:54 +0100

    Suppression de l'app.module + Correction des titres de pages

--- pokedex-app - standalone/src/app/app.component.ts
+++ pokedex-app - standalone/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  standalone: false
-  
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    imports: [RouterOutlet]
 })
 export class AppComponent { }
--- pokedex-app - standalone/src/main.ts
+++ pokedex-app - standalone/src/main.ts
@@ -1,7 +1,40 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './app/in-memory-data.service';
+import { provideRouter, Routes } from '@angular/router';
+import { AuthGuard } from './app/auth.guard';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        loadChildren: () => import("./app/pokemon/pokemon.routes")
+    },
+    {
+        path: 'login',
+        title: 'Login',
+        loadComponent: () => import("./app/login/login.component").then(module => module.LoginComponent)
+    },
+    {
+        path: '**',
+        title: 'Page Not Found',
+        loadComponent: () => import("./app/page-not-found/page-not-found.component").then(module => module.PageNotFoundComponent)
+    }
+];
+
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        importProvidersFrom(BrowserModule, FormsModule, HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })),
+        provideRouter(routes)
+    ]
 })
   .catch(err => console.error(err));
